7ea19e452f31b4b3337444c26b6f65d981e7a36e
Sylvain Peigney <sylvain@volpeo.fr>
2014-03-30 14:56:48 +0200

    gameover text

--- dist/app.js
+++ dist/app.js
@@ -241,11 +241,11 @@
 
   gameover_state = {
     create: function() {
-      var best, best_score, best_score_label, button, current, current_score, current_score_label, style, x, y;
+      var argument, argument_style, best, best_score, best_score_label, button, current, current_score, current_score_label, style, x, y;
       this.background = game.add.tileSprite(0, 0, 2000, win_height, "game-over");
       button = game.add.button(game.world.centerX - 75, 550, 'retry', this.start, this, 2, 1, 0);
       style = {
-        font: "bold 30px Verdana",
+        font: "bold 20px Verdana",
         fill: "#F9B410",
         stroke: "#4D3305",
         strokeThickness: 5
@@ -254,13 +254,22 @@
       y = game.world.height / 2;
       best = window.localStorage.getItem("nutribird-score");
       current = window.localStorage.getItem("nutribird-current-score");
-      best_score_label = this.game.add.text(x, y + 20, "MEILLEUR SCORE", style);
+      argument_style = {
+        font: "bold 20px Verdana",
+        fill: "#FFF",
+        stroke: "#4D3305",
+        strokeThickness: 5,
+        align: "center"
+      };
+      argument = this.game.add.text(x, y + 0, "Rapelles-toi, \nPicAssiette doit bouger \net manger équilibré", argument_style);
+      argument.anchor.setTo(0.5, 0.5);
+      best_score_label = this.game.add.text(x, y + 90, "MEILLEUR SCORE", style);
       best_score_label.anchor.setTo(0.5, 0.5);
-      best_score = this.game.add.text(x, y + 60, best, style);
+      best_score = this.game.add.text(x, y + 120, best, style);
       best_score.anchor.setTo(0.5, 0.5);
-      current_score_label = this.game.add.text(x, y + 130, "TON SCORE", style);
+      current_score_label = this.game.add.text(x, y + 160, "TON SCORE", style);
       current_score_label.anchor.setTo(0.5, 0.5);
-      current_score = this.game.add.text(x, y + 170, current, style);
+      current_score = this.game.add.text(x, y + 190, current, style);
       return current_score.anchor.setTo(0.5, 0.5);
     },
     start: function() {
--- scripts/gameover.coffee
+++ scripts/gameover.coffee
@@ -3,7 +3,7 @@ gameover_state =
     this.background = game.add.tileSprite(0, 0, 2000, win_height, "game-over")
     button = game.add.button(game.world.centerX - 75, 550, 'retry', this.start, this, 2, 1, 0);
 
-    style = { font: "bold 30px Verdana", fill: "#F9B410", stroke: "#4D3305", strokeThickness: 5 }
+    style = { font: "bold 20px Verdana", fill: "#F9B410", stroke: "#4D3305", strokeThickness: 5 }
     
     x = game.world.width/2
     y = game.world.height/2
@@ -11,17 +11,20 @@ gameover_state =
     best = window.localStorage.getItem("nutribird-score")
     current = window.localStorage.getItem("nutribird-current-score")
 
+    argument_style = { font: "bold 20px Verdana", fill: "#FFF", align: "center" }
+    argument = this.game.add.text(x, y + 0, "Rapelles-toi, \nPicAssiette doit bouger \net manger équilibré", argument_style)
+    argument.anchor.setTo 0.5, 0.5
 
-    best_score_label = this.game.add.text(x, y + 20, "MEILLEUR SCORE", style)
+    best_score_label = this.game.add.text(x, y + 90, "MEILLEUR SCORE", style)
     best_score_label.anchor.setTo 0.5, 0.5
 
-    best_score = this.game.add.text x, y + 60, best, style
+    best_score = this.game.add.text x, y + 120, best, style
     best_score.anchor.setTo 0.5, 0.5
 
-    current_score_label = this.game.add.text x, y + 130, "TON SCORE", style
+    current_score_label = this.game.add.text x, y + 160, "TON SCORE", style
     current_score_label.anchor.setTo 0.5, 0.5
 
-    current_score = this.game.add.text x, y + 170, current, style
+    current_score = this.game.add.text x, y + 190, current, style
     current_score.anchor.setTo 0.5, 0.5
 
   start: () ->
